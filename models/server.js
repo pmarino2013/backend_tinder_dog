e629627de51a7b66118f123a7e13c000f8a30c97
Pablo Marino <pmarino2013@gmail.com>
2022-04-06 21:35:51 -0300

    Validar token

--- models/server.js
+++ models/server.js
@@ -6,6 +6,7 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.authPath = "/api/auth";
     this.usuariosPath = "/api/usuarios";
 
     //conectar BD
@@ -38,6 +39,7 @@ class Server {
 
   //Rutas
   routes() {
+    this.app.use(this.authPath, require("../routes/auth"));
     this.app.use(this.usuariosPath, require("../routes/usuarios"));
   }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
 const {
   esRoleValido,
   emailExiste,
@@ -48,6 +49,7 @@ router.put(
 router.delete(
   "/:id",
   [
+    validarJWT,
     check("id", "No es un ID válido").isMongoId(),
     check("id").custom(usuarioExiste),
     validarCampos,
